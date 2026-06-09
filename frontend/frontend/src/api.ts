b64b3fd8057f38f8bd9dc83f8c1f0b0ea0cc560d
Chris Richardson <cjr1977@hotmail.co.uk>
2026-06-09 18:57:00 +0100

    tmp: move remove frontend/

--- frontend/frontend/src/api.ts
+++
@@ -1,8 +0,0 @@
-
-import axios, { type AxiosInstance } from "axios";
-
-const api: AxiosInstance = axios.create({
-  baseURL: `http://${import.meta.env.VITE_API_HOST_ORIGIN}:8000`,
-})
-
-export default api;
